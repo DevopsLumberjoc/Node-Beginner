e84a454e210b95874c5a2917e15967a5ccd145dc
Jacques <jacques.beauvoir@gliffy.com>
2018-11-20 19:27:51 -0800

    feat: added GET method; handles home page requests/input sent by the HTML form

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ var app = express();
 // Add functionality to handle static files
 app.use(express.static('public'));
 
+
+app.get('/index.htm', function(req, res) {
+  res.sendFile( __dirname + "/" + "index.htm");
+})
+
+app.get('/process_get', function(req, res) {
+    // Prepare output in JSON format
+    response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
 // This responds with "Hello World" on the homepage
 app.get('/', function(req, res) {
     console.log("Got a GET request for the homepage")
